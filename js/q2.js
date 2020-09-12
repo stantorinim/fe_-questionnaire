33c3f87098eeff705de84b6d9e6830babde2b94a
hlmy <hezuo@1758.com>
2020-09-13 07:01:01 +0800

    1. 更换data

--- js/q2.js
+++ js/q2.js
@@ -1,80 +1,183 @@
 const data = {
   "description": "试卷描述",
-  "id": 1,
-  "name": "试卷名称",
-  "recommend": "试卷北张庄",
+  "id": 9223372036854775807,
+  "name": "孩子学习动力评估量表",
+  "recommend": "试卷介绍",
   "subjects": [
-
     {
-      "answers": [
-        "男"
-      ],
+      "answers": [],
       "isNecessary": 1,
       "items": [
-        "男",
-        "女"
+        "非常符合",
+        "比较符合",
+        "不太符合",
+        "不符合"
       ],
-      "subjectGroup": "未命名分组",
-      "subjectId": 3,
-      "subjectName": "请选择你的性别",
-      "subjectNum": 3,
-      "subjectScore": 7,
-      "subjectTips": "答题提示xxx",
+      "subjectGroup": "",
+      "subjectId": 9223372036854775107,
+      "subjectName": "孩子的学习动力不足，主动性差，需要家长和老师经常盯着才完成学习任务，感觉总是不情愿。",
+      "subjectNum": 1,
+      "subjectScore": 11,
+      "subjectTips": "",
       "type": 3,
       "type_desc": "单选框"
     },
     {
-      "answers": [
-        "苹果",
-        "华为"
-      ],
+      "answers": [],
       "isNecessary": 1,
       "items": [
-        "苹果",
-        "华为",
-        "小米",
-        "魅族"
+        "非常符合",
+        "比较符合",
+        "不太符合",
+        "不符合"
       ],
-      "subjectGroup": "未命名分组",
-      "subjectId": 4,
-      "subjectName": "请选择你想要的手机",
-      "subjectNum": 4,
-      "subjectScore": 8,
-      "subjectTips": "答题提示xxx",
-      "type": 4,
-      "type_desc": "复选框"
+      "subjectGroup": "",
+      "subjectId": 9223372036854775108,
+      "subjectName": "孩子在自主学习（如写作业）时候，总是不能集中注意力，经常走神，磨蹭。",
+      "subjectNum": 2,
+      "subjectScore": 11,
+      "subjectTips": "",
+      "type": 3,
+      "type_desc": "单选框"
     },
-
+    {
+      "answers": [],
+      "isNecessary": 1,
+      "items": [
+        "非常符合",
+        "比较符合",
+        "不太符合",
+        "不符合"
+      ],
+      "subjectGroup": "",
+      "subjectId": 9223372036854775109,
+      "subjectName": "孩子基本不能按规定的时间完成学习任务、目标或计划，并且每次都会拖延较长时间（拖延30分钟以上）。",
+      "subjectNum": 3,
+      "subjectScore": 11,
+      "subjectTips": "",
+      "type": 3,
+      "type_desc": "单选框"
+    },
+    {
+      "answers": [],
+      "isNecessary": 1,
+      "items": [
+        "非常符合",
+        "比较符合",
+        "不太符合",
+        "不符合"
+      ],
+      "subjectGroup": "",
+      "subjectId": 9223372036854775110,
+      "subjectName": "在陪伴或指导孩子学习的过程中，孩子总显得不愿配合，如：容易着急、焦虑、甚至是有对抗家长的情绪或行为。",
+      "subjectNum": 4,
+      "subjectScore": 12,
+      "subjectTips": "",
+      "type": 3,
+      "type_desc": "单选框"
+    },
+    {
+      "answers": [],
+      "isNecessary": 1,
+      "items": [
+        "非常符合",
+        "比较符合",
+        "不太符合",
+        "不符合"
+      ],
+      "subjectGroup": "",
+      "subjectId": 9223372036854775111,
+      "subjectName": "孩子喜欢各类电子游戏（如手机/Pad/电视游戏），只要有时间就玩，经常用游戏来填充自己的空余时间，并且沉浸其中。",
+      "subjectNum": 5,
+      "subjectScore": 11,
+      "subjectTips": "",
+      "type": 3,
+      "type_desc": "单选框"
+    },
+    {
+      "answers": [],
+      "isNecessary": 1,
+      "items": [
+        "非常符合",
+        "比较符合",
+        "不太符合",
+        "不符合"
+      ],
+      "subjectGroup": "",
+      "subjectId": 9223372036854775112,
+      "subjectName": "孩子一玩起电子游戏就停不下来，甚至耽误学习以及日常休息时间，父母很难收回孩子的游戏工具，孩子对抗情绪很大，甚至经常与父母争吵",
+      "subjectNum": 6,
+      "subjectScore": 11,
+      "subjectTips": "",
+      "type": 3,
+      "type_desc": "单选框"
+    },
+    {
+      "answers": [],
+      "isNecessary": 1,
+      "items": [
+        "非常符合",
+        "比较符合",
+        "不太符合",
+        "不符合"
+      ],
+      "subjectGroup": "",
+      "subjectId": 9223372036854775113,
+      "subjectName": "父母中至少有一方，在教育孩子的过程中，总是情绪不平和，容易急躁、焦虑，或者对孩子有较大期待，并总担心孩子达不成。",
+      "subjectNum": 7,
+      "subjectScore": 11,
+      "subjectTips": "",
+      "type": 3,
+      "type_desc": "单选框"
+    },
+    {
+      "answers": [],
+      "isNecessary": 1,
+      "items": [
+        "非常符合",
+        "比较符合",
+        "不太符合",
+        "不符合"
+      ],
+      "subjectGroup": "",
+      "subjectId": 9223372036854775114,
+      "subjectName": "总的来说，父母在对孩子的教育方式中，批评多于表扬，否定多于认可。",
+      "subjectNum": 8,
+      "subjectScore": 11,
+      "subjectTips": "",
+      "type": 3,
+      "type_desc": "单选框"
+    },
+    {
+      "answers": [],
+      "isNecessary": 1,
+      "items": [
+        "非常符合",
+        "比较符合",
+        "不太符合",
+        "不符合"
+      ],
+      "subjectGroup": "",
+      "subjectId": 9223372036854775115,
+      "subjectName": "父母之间关系不和谐，经常争吵。或者父母之间至少有一方长期不在孩子身边陪伴（如长期出差，一个月回家1-2次）。",
+      "subjectNum": 9,
+      "subjectScore": 11,
+      "subjectTips": "",
+      "type": 3,
+      "type_desc": "单选框"
+    },
     {
       "answers": [],
       "isNecessary": 1,
       "items": [],
-      "subjectGroup": "未命名分组",
-      "subjectId": 6,
-      "subjectName": "下面应该是个文本块",
-      "subjectNum": 6,
-      "subjectScore": 2,
-      "subjectTips": "答题提示xxx",
+      "subjectGroup": "",
+      "subjectId": 9223372036854775116,
+      "subjectName": "对于孩子的学习和成长，您最担心的是什么?(开放题)",
+      "subjectNum": 10,
+      "subjectScore": 0,
+      "subjectTips": "",
       "type": 6,
-      "type_desc": "文本块"
-    },
-    {
-      "answers": [],
-      "isNecessary": 1,
-      "items": [
-        "标签一",
-        "标签2",
-        "标签3",
-        "标签4"
-      ],
-      "subjectGroup": "未命名分组",
-      "subjectId": 7,
-      "subjectName": "这应该是个标签组",
-      "subjectNum": 7,
-      "subjectScore": 3,
-      "subjectTips": "答题提示xxx",
-      "type": 7,
-      "type_desc": "标签组"
+      "type_desc": "单选框"
     }
   ],
   "totalScore": 100
